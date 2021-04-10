b9daf5c92ffed3b56393486f0d4568b530a4a405
Ryan Nieves <ryan.nieves@lambdas.com>
2021-04-10 17:04:11 -0400

    Hood-1 first commit

---
+++ tinder-clone2/src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Header.css"
+
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import FormIcon from '@material-ui/icons/Forum';
+
+function Header(){
+	return <div className='header'>
+		  <IconButton>
+		  <PersonIcon fontSize="large" className="header_icon"/> 
+		  </IconButton>
+	
+		<img 
+		className="header_logo" 
+		src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png" 
+		alt="" 
+		/>
+		<IconButton>
+		<FormIcon fontSize="large" className="header_icon"/>
+		</IconButton>
+		</div>
+		
+		
+		
+	
+}
+
+export default Header
